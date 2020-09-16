1d3bfb8f088b6d3c159e3e2e49f4af982e7ae4a7
Samir <sbenfares@gmail.com>
2020-09-16 16:12:01 +0200

    Remove useless file

--- server/src/jobs/import-psup/import.js
+++
@@ -1,8 +0,0 @@
-const logger = require("../../common/logger");
-const { Questionnaire, User } = require("../../common/model");
-
-module.exports = async (db) => {
-  await Questionnaire.deleteMany({});
-  await User.deleteMany({});
-  logger.info(`Questionnaires & Users successfully cleared in db ${db.name}`);
-};

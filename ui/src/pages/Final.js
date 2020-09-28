9e7cb83d301e4cadb27d9f823f75a053026e15c2
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-09-28 20:21:57 +0200

    typo

--- ui/src/pages/Final.js
+++ ui/src/pages/Final.js
@@ -8,7 +8,7 @@ export default () => {
     <Col className='m-5 d-flex flex-column flex-column'>
       <Title>Merci Prénom N. d'avoir complété votre profil !</Title>
       <Title className='mt-3 mb-5'>Je le transfère tout de suite aux entreprises que j'ai identifié pour vous.</Title>
-      <Button>Sauvegarder mon profile</Button>
+      <Button>Sauvegarder mon profil</Button>
     </Col>
   )
 }

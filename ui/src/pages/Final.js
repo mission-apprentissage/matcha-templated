76f63311f44400f38dff11d5a5e15bb1af81ebdd
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-09-28 20:08:33 +0200

    update final page

--- ui/src/pages/Final.js
+++ ui/src/pages/Final.js
@@ -5,7 +5,7 @@ import { Button, Title } from '../components'
 
 export default () => {
   return (
-    <Col className='m-5 d-flex flex-column justify-content-center flex-column'>
+    <Col className='m-5 d-flex flex-column flex-column'>
       <Title>Merci Prénom N. d'avoir complété votre profil !</Title>
       <Title className='mt-3 mb-5'>Je le transfère tout de suite aux entreprises que j'ai identifié pour vous.</Title>
       <Button>Sauvegarder mon profile</Button>

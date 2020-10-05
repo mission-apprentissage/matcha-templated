820db4c4f957546d4064b6f2345f10e6be94005d
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-05 11:40:29 +0200

    confirm window location

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,13 +6,12 @@ import ReactGA from 'react-ga'
 import { hotjar } from 'react-hotjar'
 import App from './App'
 
-console.log(window.location.hostname)
 if (process.env.NODE_ENV !== 'development') {
-  if (window.location.hostname === 'matcha') {
+  if (window.location.hostname === 'matcha.apprentissage.beta.gouv.fr') {
     ReactGA.initialize('UA-178733721-1')
     hotjar.initialize('2024584', '6')
   }
-  if (window.location.hostname === 'matcha-recette') {
+  if (window.location.hostname === 'matcha-recette.apprentissage.beta.gouv.fr') {
     hotjar.initialize('2024615', '6')
   }
 }

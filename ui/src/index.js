4ada681811a0ee735974980fcb39d53614891edf
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-05 11:27:01 +0200

    add hotjar

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './context'
 import ReactGA from 'react-ga'
+import { hotjar } from 'react-hotjar'
 import App from './App'
 
-if (process.env.NODE_ENV !== 'development') ReactGA.initialize('UA-178733721-1')
+if (process.env.NODE_ENV !== 'development') {
+  ReactGA.initialize('UA-178733721-1')
+  hotjar.initialize('2024584', '6')
+}
 
 ReactDOM.render(
   <BrowserRouter>

54b79714b6eb53b08ac784de68842f5e46468e0e
Kevin Barns <kevbarns@MacBook-Pro-de-Kevin.local>
2020-10-08 10:36:58 +0200

    missing file update GTM

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,18 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './context'
-import { hotjar } from 'react-hotjar'
 import TagManager from 'react-gtm-module'
 import App from './App'
-
-TagManager.initialize({ gtmId: 'GTM-WPC8P2B' })
 
 if (process.env.NODE_ENV !== 'development') {
   if (window.location.hostname === 'matcha.apprentissage.beta.gouv.fr') {
     TagManager.initialize({ gtmId: 'GTM-WPC8P2B' })
   }
   if (window.location.hostname === 'matcha-recette.apprentissage.beta.gouv.fr') {
-    hotjar.initialize('2024615', '6')
+    TagManager.initialize({ gtmId: 'GTM-TCBGT84' })
   }
 }
 

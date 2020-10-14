a7488efe1ad392a0f5c58dc51a6797aa6d83a439
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-14 10:14:20 +0200

    add auto redirect on homepage

--- ui/src/pages/NotLooking.js
+++ ui/src/pages/NotLooking.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { Col } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 import { Title, ExternalLink, RadioButton } from '../components'
+import styled from 'styled-components'
+
+const StyledStrong = styled.strong`
+  font-family: marianne-bold;
+`
 
 export default () => {
   let history = useHistory()
@@ -9,18 +14,23 @@ export default () => {
     <Col className='d-flex justify-content-start flex-column mt-5'>
       <Title>
         Vous êtes à la recherche
-        <br /> d'un <strong>Stage</strong> ou d'un{' '}
-        <strong>
+        <br /> d'un <StyledStrong>Stage</StyledStrong> ou d'un{' '}
+        <StyledStrong>
           contrat de
           <br />
           professionnalisation ?
-        </strong>
+        </StyledStrong>
       </Title>
       <Title>Découvrez les entreprises qui recrutent régulièrement :</Title>
-      <ExternalLink onClick={() => (window.location.href = 'https://labonnealternance.pole-emploi.fr/recherche')}>
+      <ExternalLink
+        onClick={() => (window.location.href = 'https://labonnealternance.pole-emploi.fr/recherche')}
+        className='gtm-lbabutton-notlooking'
+      >
         C'est parti
       </ExternalLink>
-      <RadioButton onClick={() => history.push('/au-revoir')}>Peut être une autre fois</RadioButton>
+      <RadioButton onClick={() => history.push('/au-revoir')} className='gtm-nextbutton-notlooking'>
+        Peut être une autre fois
+      </RadioButton>
     </Col>
   )
 }

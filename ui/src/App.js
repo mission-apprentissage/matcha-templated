897ae6e0f8a2046f7a8a9a4223fb7c646775eca1
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-20 16:38:47 +0200

    add route

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Layout } from './components'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { NotFound } from './pages'
 import routes from './routes'
 
 import './App.css'
+import Admin from './pages/Admin'
+import useAuth from './common/hooks/useAuth'
+import Login from './pages/Login'
+
+function PrivateRoute({ children, ...rest }) {
+  let [auth] = useAuth()
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return auth.sub !== 'anonymous' ? children : <Redirect to='/login' />
+      }}
+    />
+  )
+}
 
 const App = () => {
   return (
@@ -13,6 +29,8 @@ const App = () => {
         {routes.map(({ path, component }, key) => (
           <Route exact path={path} key={key} component={component} />
         ))}
+        <PrivateRoute exact path='/admin' component={Admin} />
+        <Route exact path='/login' component={Login} />
         <Route component={NotFound} />
       </Switch>
     </Layout>

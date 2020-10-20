52e15795e495467650727b3d4bf5ef350c699144
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-20 18:21:56 +0200

    add createAdmin fn

--- server/src/jobs/seed/createUsers.js
+++ server/src/jobs/seed/createUsers.js
@@ -1,10 +1,47 @@
+const passwordGenerator = require("generate-password");
 const logger = require("../../common/logger");
+const { asyncForEach } = require("../../common/utils/asyncUtils");
+const { writeFile } = require("fs").promises;
+const path = require("path");
+const config = require("config");
+
+const outputDir = config.outputDir;
+
+const passwordOptions = {
+  length: 12,
+  numbers: true,
+};
+
+const mnaUsers = [
+  {
+    username: "mna",
+    password: passwordGenerator.generate(passwordOptions),
+    writeable: true,
+    isAdmin: true,
+  },
+];
+
+const prettify = (array) => JSON.stringify(array, null, 2);
+
+const buildAdminsUsers = async (exportJsonFile = true) => {
+  exportJsonFile && (await writeFile(path.join(outputDir, "usersMna.json"), prettify(mnaUsers), "utf8"));
+  return mnaUsers;
+};
 
 module.exports = async (users) => {
-  await users.createUser("user", "name", "123456", "user@test.fr", "0611511900", "password");
-  await users.createUser("admin", "name", "78910", "admin@test.fr", "0611511901", "password", {
-    permissions: { isAdmin: true },
+  logger.info(`-- START - Seed Default Users --`);
+
+  const defaultUsersToCreate = await buildAdminsUsers();
+
+  // Users creation
+  await asyncForEach(defaultUsersToCreate, async ({ username, password, writeable, isAdmin }) => {
+    try {
+      let permissions = { writeable, isAdmin };
+      await users.createAdmin(username, password, { permissions });
+    } catch (e) {
+      logger.error(e);
+    }
   });
-  logger.info(`User 'testUser' with password 'password' is successfully created `);
-  logger.info(`User 'testAdmin' with password 'password' and admin is successfully created `);
+
+  logger.info("-- END - Seed Default Users --");
 };

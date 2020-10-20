6f31d5f37a30e3ad6ef094719168e139d2bda55c
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-20 16:38:24 +0200

    admin page

---
+++ ui/src/pages/Admin.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useFetch } from '../common/hooks/useFetch'
+
+export default () => {
+  const [data, loading] = useFetch('/api/questionnaire/items')
+  console.log(data)
+
+  return (
+    <>
+      {loading && <div>Chargement des données....</div>}
+      {data && (
+        <div>
+          {data.map(({ candidat, mobilite, voeux }, index) => {
+            return (
+              <div key={index}>
+                <p className='mb-0'>
+                  {candidat.prenom} — {candidat.nom}{' '}
+                </p>
+                <p className='mb-0'>
+                  {candidat.email} — {candidat.telephone}
+                </p>
+                <p>
+                  {mobilite && mobilite.commune} — {mobilite && mobilite.distance.label}
+                </p>
+                <p className=''>Voeux :</p>
+                {voeux &&
+                  voeux.map((voeu, index) => {
+                    return (
+                      <ul key={index}>
+                        <li>
+                          <p className='mb-0'>Formation : {voeu.formation}</p>
+                          <p className='mb-0'>Etablissement : {voeu.etablissement}</p>
+                          <p className='mb-0'>Metier visé: {voeu.metier.label}</p>
+                        </li>
+                      </ul>
+                    )
+                  })}
+                <hr />
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </>
+  )
+}

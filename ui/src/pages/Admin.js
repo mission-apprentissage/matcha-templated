dc9472e1f7e6d29091ef36c900c9f5417b4390c1
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-21 10:35:13 +0200

    update admin page

--- ui/src/pages/Admin.js
+++ ui/src/pages/Admin.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useFetch } from '../common/hooks/useFetch'
+import moment from 'moment'
 
 export default () => {
-  const [data, loading] = useFetch('/api/questionnaire/items')
+  let [data, loading] = useFetch('/api/questionnaire/items')
   console.log(data)
 
   return (
@@ -10,15 +11,14 @@ export default () => {
       {loading && <div>Chargement des données....</div>}
       {data && (
         <div>
-          {data.map(({ candidat, mobilite, voeux }, index) => {
+          {data.map(({ candidat, mobilite, voeux, experiences, recommandations, activites }, index) => {
             return (
               <div key={index}>
                 <p className='mb-0'>
-                  {candidat && candidat.prenom && candidat.prenom} — {candidat && candidat.nom && candidat.nom}{' '}
+                  {candidat && candidat.prenom} {candidat && candidat.nom}{' '}
                 </p>
                 <p className='mb-0'>
-                  {candidat && candidat.email && candidat.email} —{' '}
-                  {candidat && candidat.telephone && candidat.telephone}
+                  {candidat && candidat.email} — {candidat && candidat.telephone}
                 </p>
                 <p>
                   {mobilite && mobilite.commune && mobilite.commune} —{' '}
@@ -33,6 +33,63 @@ export default () => {
                           <p className='mb-0'>Formation : {voeu.formation}</p>
                           <p className='mb-0'>Etablissement : {voeu.etablissement}</p>
                           <p className='mb-0'>Metier visé: {voeu.metier.label}</p>
+                        </li>
+                      </ul>
+                    )
+                  })}
+                <p className=''>Expériences :</p>
+                {experiences &&
+                  experiences.map((experience, index) => {
+                    return (
+                      <ul key={index}>
+                        <li>
+                          <p className='mb-0'>
+                            {experience.nom} du {moment(experience.dateDebut).format('DD/MM/YYYY')} au{' '}
+                            {moment(experience.dateFin).format('DD/MM/YYYY')}
+                          </p>
+                          <p className='mb-0'>
+                            Entreprise : {experience.nomEntreprise} — {experience.adresseEntreprise}
+                          </p>
+                          <p className='mb-0'>Tâches :</p>
+                          {experience.taches.map((tache, index) => {
+                            return <ul key={index}>{tache} </ul>
+                          })}
+                          <p></p>
+                        </li>
+                      </ul>
+                    )
+                  })}
+                <p className=''>Activités :</p>
+                {activites &&
+                  activites.map((activite, index) => {
+                    return (
+                      <ul key={index}>
+                        <li>
+                          <p className='mb-0'>
+                            {activite.nom} - {activite.periodicite}
+                          </p>
+                          <p className='mb-0'>Critères :</p>
+                          {activite.criteres &&
+                            activite.criteres.map((critere, index) => {
+                              return <ul key={index}>{critere}</ul>
+                            })}
+                        </li>
+                      </ul>
+                    )
+                  })}
+                <p className=''>Recommandations :</p>
+                {recommandations &&
+                  recommandations.map((reco, index) => {
+                    return (
+                      <ul>
+                        <li>
+                          <p className='mb-0'>
+                            {reco.prenom} {reco.nom}
+                          </p>
+                          <p className='mb-0'>
+                            {reco.email} {reco.telephone}
+                          </p>
+                          <p>Role : {reco.role}</p>
                         </li>
                       </ul>
                     )

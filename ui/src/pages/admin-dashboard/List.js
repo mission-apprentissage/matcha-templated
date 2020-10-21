387c9325534e63b3e1719b6e4a32dadc1c08f87f
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-10-21 11:15:42 +0200

    update list display

--- ui/src/pages/admin-dashboard/List.js
+++ ui/src/pages/admin-dashboard/List.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { useFetch } from '../../common/hooks/useFetch'
-import { Table } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 export default function List() {
   let [data, loading] = useFetch('/api/questionnaire/items')
   console.log(data)
   return (
-    <div>
+    <>
       {loading && <div>Chargement des données....</div>}
-      <Table>
+      <Table striped responsive='lg'>
         <thead>
           <th>Nom</th>
           <th>Prénom</th>
           <th>Email</th>
           <th>Téléphone</th>
+          <th>Date de création</th>
           <th>Lien</th>
         </thead>
         <tbody>
@@ -27,14 +29,15 @@ export default function List() {
                   <td>{item.candidat.prenom}</td>
                   <td>{item.candidat.email}</td>
                   <td>{item.candidat.telephone}</td>
+                  <td>{moment(item.createdAt).format('DD/MM/YYYY')}</td>
                   <td>
-                    <Link to={`/admin/${item._id}`}>Voir le profile</Link>
+                    <Link to={`/admin/${item._id}`}>Détails</Link>
                   </td>
                 </tr>
               )
             })}
         </tbody>
       </Table>
-    </div>
+    </>
   )
 }

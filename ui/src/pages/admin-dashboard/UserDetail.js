2238ebcf87f6fd7f590204cf02aeff584aa12345
Kevin Barns <kevbarns@MacBook-Pro-de-Kevin.local>
2020-11-04 11:49:10 +0100

    add edit button

--- ui/src/pages/admin-dashboard/UserDetail.js
+++ ui/src/pages/admin-dashboard/UserDetail.js
@@ -13,95 +13,102 @@ export default (props) => {
       {loading && <div>Chargement des données....</div>}
       {data && (
         <div>
-          {data.map(({ candidat, mobilite, voeux, experiences, recommandations, activites }, index) => {
-            return (
-              <div key={index}>
-                <p className='mb-0'>
-                  {candidat && candidat.prenom} {candidat && candidat.nom}{' '}
-                </p>
-                <p className='mb-0'>
-                  {candidat && candidat.email} — {candidat && candidat.telephone}
-                </p>
-                <p>
-                  {mobilite && mobilite.commune && mobilite.commune} —{' '}
-                  {mobilite && mobilite.distance.label && mobilite.distance.label}
-                </p>
-                <p className=''>Voeux :</p>
-                {voeux &&
-                  voeux.map((voeu, index) => {
-                    return (
-                      <ul key={index}>
-                        <li>
-                          <p className='mb-0'>Formation : {voeu.formation}</p>
-                          <p className='mb-0'>Etablissement : {voeu.etablissement}</p>
-                          <p className='mb-0'>Metier visé: {voeu.metier.label}</p>
-                        </li>
-                      </ul>
-                    )
-                  })}
-                <p className=''>Expériences :</p>
-                {experiences &&
-                  experiences.map((experience, index) => {
-                    return (
-                      <ul key={index}>
-                        <li>
-                          <p className='mb-0'>
-                            {experience.nom} du {moment(experience.dateDebut).format('DD/MM/YYYY')} au{' '}
-                            {moment(experience.dateFin).format('DD/MM/YYYY')}
-                          </p>
-                          <p className='mb-0'>
-                            Entreprise : {experience.nomEntreprise} — {experience.adresseEntreprise}
-                          </p>
-                          <p className='mb-0'>Tâches :</p>
-                          {experience.taches.map((tache, index) => {
-                            return <ul key={index}>{tache} </ul>
-                          })}
-                          <p></p>
-                        </li>
-                      </ul>
-                    )
-                  })}
-                <p className=''>Activités :</p>
-                {activites &&
-                  activites.map((activite, index) => {
-                    return (
-                      <ul key={index}>
-                        <li>
-                          <p className='mb-0'>
-                            {activite.nom} - {activite.periodicite}
-                          </p>
-                          <p className='mb-0'>Critères :</p>
-                          {activite.criteres &&
-                            activite.criteres.map((critere, index) => {
-                              return <ul key={index}>{critere}</ul>
+          {data.map(
+            ({ candidat, mobilite, voeux, experiences, recommandations, activites, questionnaire_id }, index) => {
+              return (
+                <div key={index}>
+                  <p className='mb-0'>
+                    {candidat && candidat.prenom} {candidat && candidat.nom}{' '}
+                  </p>
+                  <p className='mb-0'>
+                    {candidat && candidat.email} — {candidat && candidat.telephone}
+                  </p>
+                  <p>
+                    {mobilite && mobilite.commune && mobilite.commune} —{' '}
+                    {mobilite && mobilite.distance.label && mobilite.distance.label}
+                  </p>
+                  <p className=''>Voeux :</p>
+                  {voeux &&
+                    voeux.map((voeu, index) => {
+                      return (
+                        <ul key={index}>
+                          <li>
+                            <p className='mb-0'>Formation : {voeu.formation}</p>
+                            <p className='mb-0'>Etablissement : {voeu.etablissement}</p>
+                            <p className='mb-0'>Metier visé: {voeu.metier.label}</p>
+                          </li>
+                        </ul>
+                      )
+                    })}
+                  <p className=''>Expériences :</p>
+                  {experiences &&
+                    experiences.map((experience, index) => {
+                      return (
+                        <ul key={index}>
+                          <li>
+                            <p className='mb-0'>
+                              {experience.nom} du {moment(experience.dateDebut).format('DD/MM/YYYY')} au{' '}
+                              {moment(experience.dateFin).format('DD/MM/YYYY')}
+                            </p>
+                            <p className='mb-0'>
+                              Entreprise : {experience.nomEntreprise} — {experience.adresseEntreprise}
+                            </p>
+                            <p className='mb-0'>Tâches :</p>
+                            {experience.taches.map((tache, index) => {
+                              return <ul key={index}>{tache} </ul>
                             })}
-                        </li>
-                      </ul>
-                    )
-                  })}
-                <p className=''>Recommandations :</p>
-                {recommandations &&
-                  recommandations.map((reco, index) => {
-                    return (
-                      <ul>
-                        <li>
-                          <p className='mb-0'>
-                            {reco.prenom} {reco.nom}
-                          </p>
-                          <p className='mb-0'>
-                            {reco.email} {reco.telephone}
-                          </p>
-                          <p>Role : {reco.role}</p>
-                        </li>
-                      </ul>
-                    )
-                  })}
-              </div>
-            )
-          })}
-          <Link to='/admin'>
-            <Button>Retour</Button>
-          </Link>
+                            <p></p>
+                          </li>
+                        </ul>
+                      )
+                    })}
+                  <p className=''>Activités :</p>
+                  {activites &&
+                    activites.map((activite, index) => {
+                      return (
+                        <ul key={index}>
+                          <li>
+                            <p className='mb-0'>
+                              {activite.nom} - {activite.periodicite}
+                            </p>
+                            <p className='mb-0'>Critères :</p>
+                            {activite.criteres &&
+                              activite.criteres.map((critere, index) => {
+                                return <ul key={index}>{critere}</ul>
+                              })}
+                          </li>
+                        </ul>
+                      )
+                    })}
+                  <p className=''>Recommandations :</p>
+                  {recommandations &&
+                    recommandations.map((reco, index) => {
+                      return (
+                        <ul>
+                          <li>
+                            <p className='mb-0'>
+                              {reco.prenom} {reco.nom}
+                            </p>
+                            <p className='mb-0'>
+                              {reco.email} {reco.telephone}
+                            </p>
+                            <p>Role : {reco.role}</p>
+                          </li>
+                        </ul>
+                      )
+                    })}
+                  <div className='d-flex justify-content-between mb-5'>
+                    <Link to='/admin'>
+                      <Button>Retour</Button>
+                    </Link>
+                    <Link to={`/edition/${questionnaire_id}`}>
+                      <Button>Accéder au questionnaire</Button>
+                    </Link>
+                  </div>
+                </div>
+              )
+            }
+          )}
         </div>
       )}
     </>

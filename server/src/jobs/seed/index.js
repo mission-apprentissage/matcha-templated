fd6eb9057a33a59f5775499da9c8f2a6b30e97dc
Kevin Barns <kevbarns@MacBook-Pro-de-Kevin.local>
2020-11-04 12:13:10 +0100

    add remove seed

--- server/src/jobs/seed/index.js
+++ server/src/jobs/seed/index.js
@@ -1,8 +1,10 @@
 const { runScript } = require("../scriptWrapper");
 const seedQuestionnaire = require("./seed");
 const createUsers = require("./createUsers");
+const removeUser = require("./removeUser");
 
 runScript(async ({ users, db }) => {
   // await seedQuestionnaire(db, users);
-  await createUsers(users);
+  // await createUsers(users);
+  await removeUser();
 });

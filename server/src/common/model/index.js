d92ab9b0bf1b63eb61b35c2a563d05228c484f52
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-01 14:34:04 +0100

    core: add es client configuration

--- server/src/common/model/index.js
+++ server/src/common/model/index.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
+const schema = require("./schema");
+const { mongoosastic, getElasticInstance } = require("../esClient");
 
-const getModel = (modelName, callback = () => ({})) => {
-  const Schema = require(`./schema/${modelName}`);
-  callback(Schema);
-  return mongoose.model(modelName, Schema, modelName);
+const createModel = (modelName, descriptor, options = {}) => {
+  const schema = new mongoose.Schema(descriptor);
+
+  if (options.esIndexName) {
+    schema.plugin(mongoosastic, { esClient: getElasticInstance(), index: options.esIndexName });
+  }
+  if (options.createMongoDBIndexes) {
+    options.createMongoDBIndexes(schema);
+  }
+  return mongoose.model(modelName, schema, options.collectionName);
 };
 
 module.exports = {
-  Questionnaire: getModel("questionnaires"),
-  User: getModel("users"),
-  Log: getModel("logs"),
+  Formulaire: createModel("formulaireOPCO", schema.formulaireOPCOSchema, {
+    esIndexName: "formulaireopco",
+  }),
+  Questionnaire: createModel("questionnaires", schema.questionnairesSchema),
+  User: createModel("users", schema.usersSchema),
+  Log: createModel("logs", schema.logSchema),
 };
---
+++ server/src/common/model/schema/index.js
@@ -0,0 +1,11 @@
+const formulaireOPCOSchema = require("./formulaireOPCO");
+const questionnairesSchema = require("./questionnaires");
+const logSchema = require("./logs");
+const usersSchema = require("./users");
+
+module.exports = {
+  formulaireOPCOSchema,
+  questionnairesSchema,
+  logSchema,
+  usersSchema,
+};

1a1355227031f2bf895133534b48c672861267a6
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-17 18:11:16 +0100

    feat: add timestamps

--- server/src/common/model/schema/users.js
+++ server/src/common/model/schema/users.js
@@ -1,58 +1,63 @@
 const { Schema } = require("mongoose");
 
-module.exports = new Schema({
-  username: {
-    type: String,
-    default: null,
-    description: "L'identifiant username de l'utilisateur",
-    unique: true,
+module.exports = new Schema(
+  {
+    username: {
+      type: String,
+      default: null,
+      description: "L'identifiant username de l'utilisateur",
+      unique: true,
+    },
+    nom: {
+      type: String,
+      default: null,
+      description: "Le nom de l'utilisateur",
+    },
+    prenom: {
+      type: String,
+      default: null,
+      description: "Le prénom de l'utilisateur",
+    },
+    parcoursup_id: {
+      type: String,
+      default: null,
+      description: "L'identifiant parcoursup de l'utilisateur",
+      unique: true,
+    },
+    email: {
+      type: String,
+      default: null,
+      description: "L'email de l'utilisateur",
+      unique: true,
+    },
+    telephone: {
+      type: String,
+      default: null,
+      description: "Le numéro de telephone de l'utilisateur",
+      unique: true,
+    },
+    password: {
+      type: String,
+      default: null,
+      description: "Le mot de passe hashé",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      description: "true si l'utilisateur est administrateur",
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+      description: "Date d'ajout en base de données",
+    },
+    last_update_at: {
+      type: Date,
+      default: Date.now,
+      description: "Date de dernières mise à jour",
+    },
   },
-  nom: {
-    type: String,
-    default: null,
-    description: "Le nom de l'utilisateur",
-  },
-  prenom: {
-    type: String,
-    default: null,
-    description: "Le prénom de l'utilisateur",
-  },
-  parcoursup_id: {
-    type: String,
-    default: null,
-    description: "L'identifiant parcoursup de l'utilisateur",
-    unique: true,
-  },
-  email: {
-    type: String,
-    default: null,
-    description: "L'email de l'utilisateur",
-    unique: true,
-  },
-  telephone: {
-    type: String,
-    default: null,
-    description: "Le numéro de telephone de l'utilisateur",
-    unique: true,
-  },
-  password: {
-    type: String,
-    default: null,
-    description: "Le mot de passe hashé",
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-    description: "true si l'utilisateur est administrateur",
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-    description: "Date d'ajout en base de données",
-  },
-  last_update_at: {
-    type: Date,
-    default: Date.now,
-    description: "Date de dernières mise à jour",
-  },
-});
+  {
+    timestamps: true,
+  }
+);

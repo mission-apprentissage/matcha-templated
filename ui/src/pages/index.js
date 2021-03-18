5ea4198d69a0ff62676a43c4174e437b8eeb8063
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-18 17:20:35 +0100

    fix: update path

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -5,6 +5,6 @@ import NotFound from './NotFound'
 import NotLooking from './NotLooking'
 import End from './End'
 import Edition from './Edition'
-import FormulaireOPCO from './FormulaireOPCO'
+import FormulaireOPCO from './formulaireOPCO/FormulaireOPCO'
 
 export { NotFound, Homepage, First, Final, NotLooking, End, Edition, FormulaireOPCO }

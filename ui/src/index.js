a3a5a7d71c54b295f749e45e3c88226c89ef8d43
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-20 12:39:33 +0100

    feat: add chakra-ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './context'
 import TagManager from 'react-gtm-module'
 import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 TagManager.initialize({ gtmId: 'GTM-KL849C7' })
 
 ReactDOM.render(
   <BrowserRouter>
     <ContextProvider>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </ContextProvider>
   </BrowserRouter>,
   document.getElementById('root')

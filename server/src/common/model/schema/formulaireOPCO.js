f32c123e72c9fae6df49f17609ba4fe9c8b0e1cc
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-23 16:47:42 +0100

    feat: update model

--- server/src/common/model/schema/formulaireOPCO.js
+++ server/src/common/model/schema/formulaireOPCO.js
@@ -3,49 +3,60 @@ const { nanoid } = require("nanoid");
 
 module.exports = new Schema(
   {
-    form_id: {
+    id_form: {
       type: String,
       default: () => nanoid(),
+      description: "Identifiant de formulaire unique",
     },
     raison_social: {
       type: String,
       default: null,
+      description: "Raison social de",
     },
     siret: {
       type: String,
       default: null,
+      description: "Numéro SIRET de l'entreprise",
     },
     adresse: {
       type: String,
       default: null,
+      description: "Adresse de l'entreprise",
     },
-    coordonnees_geo: {
+    coordonnees_geo_latitude: {
       type: String,
       default: null,
+      description: "Latitude de l'adresse de l'entreprise",
     },
-    ville: {
+    coordonnees_geo_longitude: {
       type: String,
       default: null,
+      description: "Longitude de l'adresse de l'entreprise",
     },
     nom: {
       type: String,
       default: null,
+      description: "Nom du contact",
     },
     prenom: {
       type: String,
       default: null,
+      description: "Prénom du contact",
     },
     telephone: {
       type: String,
       default: null,
+      description: "Téléphone du contact",
     },
     email: {
       type: String,
       default: null,
+      description: "Email du contact",
     },
     offres: {
       type: Array,
       default: [],
+      description: "Liste des métiers sur lesquels l'entreprise recherche des apprentis",
     },
   },
   {

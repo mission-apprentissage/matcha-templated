28fa3dfb62265e87d6c69bbe5ac1356a20da5fec
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-23 17:01:04 +0100

    fix: dead route & test

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -11,7 +11,6 @@ const authMiddleware = require("./middlewares/authMiddleware");
 const permissionsMiddleware = require("./middlewares/permissionsMiddleware");
 const packageJson = require("../../package.json");
 const questionnaire = require("./routes/questionnaire");
-const parcoursup = require("./routes/parcoursup");
 const secured = require("./routes/secured");
 const login = require("./routes/login");
 const authentified = require("./routes/authentified");
@@ -31,7 +30,6 @@ module.exports = async (components) => {
   app.use(logMiddleware());
 
   app.use("/api/questionnaire", questionnaire());
-  app.use("/api/parcoursup", parcoursup());
   app.use("/api/secured", apiKeyAuthMiddleware, secured());
   app.use("/api/login", login(components));
   app.use("/api/authentified", checkJwtToken, authentified());

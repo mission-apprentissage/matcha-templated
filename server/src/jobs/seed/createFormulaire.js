855ea41dfc63d70c9270e7f6dc194dea095ef759
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-25 16:45:39 +0100

    fix: dummy data

--- server/src/jobs/seed/createFormulaire.js
+++ server/src/jobs/seed/createFormulaire.js
@@ -2,13 +2,13 @@ const logger = require("../../common/logger");
 
 module.exports = async (formulaire) => {
   const payload = {
-    prenom: "John",
-    nom: "Doe",
-    telephone: "0746567686",
-    email: "k.barnoin@gmail.com",
-    raison_social: "BETA GOUV",
+    prenom: "Formulaire",
+    nom: "Test",
+    telephone: "0123456789",
+    email: "adresse@mail.com",
+    raison_social: "Ma société",
     siret: "12345678901234",
-    adresse: "3 rue du liban, 75020, Paris",
+    adresse: "3 rue george duhamel, 75012, Paris",
   };
 
   const user = await formulaire.createForm(payload);

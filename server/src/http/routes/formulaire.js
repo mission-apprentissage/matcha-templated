e92a9ca046867f37d50cadd35e547e53daad0d55
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-30 12:30:14 +0200

    feat: elastic endpoint LBA

--- server/src/http/routes/formulaire.js
+++ server/src/http/routes/formulaire.js
@@ -22,6 +22,69 @@ module.exports = () => {
   );
 
   /**
+   * LBA ENDPOINT
+   */
+  router.post(
+    "/search",
+    tryCatch(async (req, res) => {
+      const { distance, lat, lon, romes } = req.body;
+
+      if (!distance || !lat || !lon || !romes) {
+        return res.status(400).json({ error: "Arguments is missing (distance, lat, lon, romes)" });
+      }
+
+      const body = {
+        query: {
+          bool: {
+            must: [
+              {
+                nested: {
+                  path: "offres",
+                  query: {
+                    match: {
+                      "offres.romes": romes.join(" "),
+                    },
+                  },
+                },
+              },
+            ],
+            filter: [
+              {
+                geo_distance: {
+                  distance: `${distance}km`,
+                  geo_coordonnees: {
+                    lat,
+                    lon,
+                  },
+                },
+              },
+            ],
+          },
+        },
+        sort: [
+          {
+            _geo_distance: {
+              geo_coordonnees: {
+                lat,
+                lon,
+              },
+              order: "asc",
+              unit: "km",
+              mode: "min",
+              distance_type: "arc",
+              ignore_unmapped: true,
+            },
+          },
+        ],
+      };
+
+      const result = await esClient.search({ index: "formulaires", body });
+
+      return res.json(result.body.hits.hits);
+    })
+  );
+
+  /**
    * Post FORM
    */
   router.post(
@@ -35,77 +98,6 @@ module.exports = () => {
       //TODO : send thank you mail
 
       return res.sendStatus(200);
-    })
-  );
-
-  /**
-   * LBA ENDPOINT
-   */
-  router.post(
-    "/_search",
-    tryCatch(async (req, res) => {
-      const { distance, lat, lon, romes } = req.body;
-
-      let mustTerm = [];
-
-      if (romes)
-        mustTerm.push({
-          match: {
-            offres: romes.join(" "),
-          },
-        });
-
-      // not used as of now
-      // if (romeDomain)
-      //   mustTerm.push({
-      //     multi_match: {
-      //       query: romeDomain,
-      //       fields: ["rome_codes"],
-      //       type: "phrase_prefix",
-      //       operator: "or",
-      //     },
-      //   });
-
-      // TODO : normalize later is needed
-      // if (diploma)
-      //   mustTerm.push({
-      //     match: {
-      //       niveau: diploma,
-      //     },
-      //   });
-
-      const payload = {
-        query: {
-          bool: {
-            must: mustTerm,
-            filter: {
-              geo_distance: {
-                distance: `${distance}km`,
-                geo_coordonnees: {
-                  lat,
-                  lon,
-                },
-              },
-            },
-          },
-        },
-        sort: [
-          {
-            _geo_distance: {
-              geo_coordonnees: [parseFloat(lat), parseFloat(lon)],
-              order: "asc",
-              unit: "km",
-              mode: "min",
-              distance_type: "arc",
-              ignore_unmapped: true,
-            },
-          },
-        ],
-      };
-
-      const result = await esClient.search(payload);
-
-      return res.json(result.body);
     })
   );
 

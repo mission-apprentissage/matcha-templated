be81664031d4381cfd29fb97d58a49acece5f87d
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-30 12:31:24 +0200

    fix: jobs

--- server/src/jobs/formulaire/generateEsIndex.js
+++ server/src/jobs/formulaire/generateEsIndex.js
@@ -7,7 +7,7 @@ async function clearIndex() {
   try {
     let client = getElasticInstance();
     await client.indices.delete({ index: "formulaires" });
-    logger.info("Index Formulaire cleared");
+    logger.info("Index Formulaire CLEARED !");
   } catch (error) {
     logger.error(`Could not reset the index`, error);
   }
@@ -16,6 +16,7 @@ async function clearIndex() {
 async function createIndex() {
   let requireAsciiFolding = true;
   await Formulaire.createMapping(requireAsciiFolding);
+  logger.info("Index Formulaire CREATED !");
 }
 
 runScript(async () => {
--- server/src/jobs/seed/createFormulaire.js
+++ server/src/jobs/seed/createFormulaire.js
@@ -10,6 +10,14 @@ module.exports = async (formulaire) => {
     siret: "12345678901234",
     geo_coordonnees: "2.385633,48.868565",
     adresse: "3 rue george duhamel, 75012, Paris",
+    offres: [
+      {
+        niveau: "CAP, BEP",
+        romes: ["A1203", "A1414"],
+        libelle: "Paysagisme, entretien des jardins",
+        description: "Détail",
+      },
+    ],
   };
 
   const user = await formulaire.createForm(payload);

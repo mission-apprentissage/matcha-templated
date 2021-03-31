9f5ceb098ad65b390f4f6ff0090935541eef839a
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-31 10:21:20 +0200

    feat: index rebuilder job

--- server/src/jobs/formulaire/generateEsIndex.js
+++ server/src/jobs/formulaire/generateEsIndex.js
@@ -1,25 +1,7 @@
 const { Formulaire } = require("../../common/model");
-const { getElasticInstance } = require("../../common/esClient");
-const logger = require("../../common/logger");
 const { runScript } = require("../scriptWrapper");
-
-async function clearIndex() {
-  try {
-    let client = getElasticInstance();
-    await client.indices.delete({ index: "formulaires" });
-    logger.info("Index Formulaire CLEARED !");
-  } catch (error) {
-    logger.error(`Could not reset the index`, error);
-  }
-}
-
-async function createIndex() {
-  let requireAsciiFolding = true;
-  await Formulaire.createMapping(requireAsciiFolding);
-  logger.info("Index Formulaire CREATED !");
-}
+const { rebuildIndex } = require("../../common/utils/esUtils");
 
 runScript(async () => {
-  await clearIndex();
-  await createIndex();
+  await rebuildIndex("formulaires", Formulaire);
 });

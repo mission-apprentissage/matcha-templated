cabcc7663d06e9afbe6d148f8069dfdd5a843523
Kevin Barnoin <k.barnoin@gmail.com>
2021-04-06 09:43:45 +0200

    fix: fn definition

--- server/src/common/components/statistique.js
+++ server/src/common/components/statistique.js
@@ -1,22 +1,24 @@
 const { Formulaire } = require("../model");
 const { OPENED, CLICKED, DELIVERED, HARD, SOFT } = require("./mail.constant");
 
-module.exports = {
-  getAllStatistiques: async () => {
-    const nbForms = async () => await Formulaire.countDocuments();
-    const nbOpened = async () => await Formulaire.countDocuments({ events: { event: OPENED } });
-    const nbClicked = async () => await Formulaire.countDocuments({ events: { event: CLICKED } });
-    const nbDelivered = async () => await Formulaire.countDocuments({ events: { event: DELIVERED } });
-    const nbHard = async () => await Formulaire.countDocuments({ events: { event: HARD } });
-    const nbSoft = async () => await Formulaire.countDocuments({ events: { event: SOFT } });
+module.exports = async () => {
+  return {
+    getAllStatistiques: async () => {
+      const nbForms = async () => await Formulaire.countDocuments();
+      const nbOpened = async () => await Formulaire.countDocuments({ events: { event: OPENED } });
+      const nbClicked = async () => await Formulaire.countDocuments({ events: { event: CLICKED } });
+      const nbDelivered = async () => await Formulaire.countDocuments({ events: { event: DELIVERED } });
+      const nbHard = async () => await Formulaire.countDocuments({ events: { event: HARD } });
+      const nbSoft = async () => await Formulaire.countDocuments({ events: { event: SOFT } });
 
-    return {
-      nbHard,
-      nbSoft,
-      nbForms,
-      nbOpened,
-      nbClicked,
-      nbDelivered,
-    };
-  },
+      return {
+        nbHard,
+        nbSoft,
+        nbForms,
+        nbOpened,
+        nbClicked,
+        nbDelivered,
+      };
+    },
+  };
 };

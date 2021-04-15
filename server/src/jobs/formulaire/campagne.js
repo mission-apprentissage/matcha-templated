2cd967c989cc3976353f2c9c0bac293838d7be2f
Kevin Barnoin <k.barnoin@gmail.com>
2021-04-15 16:00:20 +0200

    feat: update mail avis

--- server/src/jobs/formulaire/campagne.js
+++ server/src/jobs/formulaire/campagne.js
@@ -4,62 +4,9 @@ const { runScript } = require("../scriptWrapper");
 const { asyncForEach } = require("../../common/utils/asyncUtils");
 const logger = require("../../common/logger");
 
-const launch_2 = async (mail) => {
-  const forms = await Formulaire.find({ "events.event": "opened", "events.tag": "matcha-lbb-20210331" }).lean();
-  const campagne = "matcha-lbb-20210412-relance-ouvert-non-click";
-
-  // const campagne = "test";
-
-  logger.info(`scope : ${forms.length} formulaires`);
-
-  await asyncForEach(forms, async (form) => {
-    const { raison_sociale, email, id_form, _id } = form;
-    const params = {
-      RAISON_SOCIALE: raison_sociale,
-      URL: `https://matcha.apprentissage.beta.gouv.fr/${id_form}`,
-    };
-    const body = {
-      sender: {
-        name: "Mission interministérielle pour l'apprentissage",
-        email: "charlotte.lecuit@beta.gouv.fr",
-      },
-      to: [
-        {
-          name: `${raison_sociale}`,
-          email: `${email}`,
-        },
-      ],
-      replyTo: {
-        name: "Charlotte Lecuit",
-        email: "charlotte.lecuit@beta.gouv.fr",
-      },
-      subject: `Relance: le gouvernement vous aide à recruter un apprenti`,
-      templateId: 171,
-      tags: [campagne],
-      params: params,
-    };
-
-    const { body: result } = await mail.sendmail(body);
-    const message = {
-      campagne,
-      code: result.code ?? null,
-      message: result.message ?? null,
-      messageId: result.messageId ?? null,
-    };
-
-    if (result.messageId) {
-      logger.info(`email sent ${email}`);
-    } else {
-      logger.info(`error : ${message.code} —— ${message.message}`);
-    }
-
-    await Formulaire.findByIdAndUpdate(_id, { $push: { mailing: message } });
-  });
-};
-
 const launch = async (mail) => {
-  const forms = await Formulaire.find({ "events.event": "delivered", "events.tag": "matcha-lbb-20210331" }).lean();
-  const campagne = "matcha-lbb-20210412-relance-non-ouvert";
+  const forms = await Formulaire.find({ $nor: [{ offres: { $exists: false } }, { offres: { $size: 0 } }] }).lean();
+  const campagne = "matcha-lbb-20210415-avis";
 
   // const campagne = "test";
 
@@ -79,6 +26,7 @@ const launch = async (mail) => {
       to: [
         {
           name: `${raison_sociale}`,
+          // email: "k.barnoin@gmail.com",
           email: `${email}`,
         },
       ],
@@ -86,8 +34,8 @@ const launch = async (mail) => {
         name: "Charlotte Lecuit",
         email: "charlotte.lecuit@beta.gouv.fr",
       },
-      subject: `Relance: le gouvernement vous aide à recruter un apprenti`,
-      templateId: 170,
+      // subject: `Relance: le gouvernement vous aide à recruter un apprenti`,
+      templateId: 173,
       tags: [campagne],
       params: params,
     };
@@ -112,5 +60,4 @@ const launch = async (mail) => {
 
 runScript(async ({ mail }) => {
   await launch(mail);
-  await launch_2(mail);
 });

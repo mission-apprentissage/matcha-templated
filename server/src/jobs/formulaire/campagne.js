3d14095b2a6a09d0474389505e36f5791f2e8e8f
Kevin Barnoin <k.barnoin@gmail.com>
2021-04-22 18:43:15 +0200

    feat: add paginator

--- server/src/jobs/formulaire/campagne.js
+++ server/src/jobs/formulaire/campagne.js
@@ -1,65 +1,64 @@
 /* eslint-disable */
 const { Formulaire } = require("../../common/model");
 const { runScript } = require("../scriptWrapper");
-const { asyncForEach } = require("../../common/utils/asyncUtils");
 const logger = require("../../common/logger");
 const { CLICKED } = require("../../common/components/mail.constant");
+const { paginator } = require("../../common/utils/paginator");
 
 const launch = async (mail) => {
-  const forms = await Formulaire.find({
-    "events.event": CLICKED,
-    $or: [{ offres: { $exists: false } }, { offres: { $size: 0 } }],
-  }).lean();
-  const campagne = "matcha-lbb-20210420-avis-click-sans-offre";
+  await paginator(
+    Formulaire,
+    {
+      "events.event": CLICKED,
+      $or: [{ offres: { $exists: false } }, { offres: { $size: 0 } }],
+    },
+    { lean: true },
+    async (form) => {
+      // const campagne = "matcha-lbb-20210420-avis-click-sans-offre";
+      const campagne = "test";
 
-  // const campagne = "test";
-
-  logger.info(`scope : ${forms.length} formulaires`);
-
-  await asyncForEach(forms, async (form) => {
-    const { raison_sociale, email, id_form, _id } = form;
-    const params = {
-      RAISON_SOCIALE: raison_sociale,
-      URL: `https://matcha.apprentissage.beta.gouv.fr/${id_form}`,
-    };
-    const body = {
-      sender: {
-        name: "Mission interministérielle pour l'apprentissage",
-        email: "charlotte.lecuit@beta.gouv.fr",
-      },
-      to: [
-        {
-          name: `${raison_sociale}`,
-          // email: "k.barnoin@gmail.com",
-          email: `${email}`,
+      const { raison_sociale, email, id_form, _id } = form;
+      const params = {
+        RAISON_SOCIALE: raison_sociale,
+        URL: `https://matcha.apprentissage.beta.gouv.fr/${id_form}`,
+      };
+      const body = {
+        sender: {
+          name: "Mission interministérielle pour l'apprentissage",
+          email: "charlotte.lecuit@beta.gouv.fr",
         },
-      ],
-      replyTo: {
-        name: "Charlotte Lecuit",
-        email: "charlotte.lecuit@beta.gouv.fr",
-      },
-      // subject: `Relance: le gouvernement vous aide à recruter un apprenti`,
-      templateId: 174,
-      tags: [campagne],
-      params: params,
-    };
+        to: [
+          {
+            name: `${raison_sociale}`,
+            email: "k.barnoin@gmail.com",
+            // email: `${email}`,
+          },
+        ],
+        replyTo: {
+          name: "Charlotte Lecuit",
+          email: "charlotte.lecuit@beta.gouv.fr",
+        },
+        // subject: `Relance: le gouvernement vous aide à recruter un apprenti`,
+        templateId: 174,
+        tags: [campagne],
+        params: params,
+      };
 
-    const { body: result } = await mail.sendmail(body);
-    const message = {
-      campagne,
-      code: result.code ?? null,
-      message: result.message ?? null,
-      messageId: result.messageId ?? null,
-    };
+      const { body: result } = await mail.sendmail(body);
+      const message = {
+        campagne,
+        code: result.code ?? null,
+        message: result.message ?? null,
+        messageId: result.messageId ?? null,
+      };
 
-    if (result.messageId) {
-      logger.info(`email sent ${email}`);
-    } else {
-      logger.info(`error : ${message.code} —— ${message.message}`);
+      if (!result.messageId) {
+        logger.info(`error : ${message.code} —— ${message.message} — ${email}`);
+      }
+
+      await Formulaire.findByIdAndUpdate(_id, { $push: { mailing: message } });
     }
-
-    await Formulaire.findByIdAndUpdate(_id, { $push: { mailing: message } });
-  });
+  );
 };
 
 runScript(async ({ mail }) => {

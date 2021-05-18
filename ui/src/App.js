41a151e3e4f670f327054f18b7f591035a53f76e
Kevin Barnoin <k.barnoin@gmail.com>
2021-05-18 22:15:27 +0200

    feat: remove react-query

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { Formulaire, Homepage, NotFound, Remerciement } from './pages'
 import { Login, UserList } from './pages/admin-dashboard'
@@ -8,8 +7,6 @@ import useAuth from './common/hooks/useAuth'
 import theme from './theme'
 
 import './App.css'
-
-const client = new QueryClient()
 
 function PrivateRoute({ children, ...rest }) {
   let [auth] = useAuth()
@@ -26,21 +23,19 @@ function PrivateRoute({ children, ...rest }) {
 
 const App = () => {
   return (
-    <QueryClientProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Switch>
-          <PrivateRoute exact path='/admin'>
-            <UserList />
-          </PrivateRoute>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/merci' component={Remerciement} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/formulaire/:id' render={(props) => <Formulaire {...props} byId={true} />} />
-          <Route strict path='/:origine/' component={Formulaire} />
-          <Route component={NotFound} />
-        </Switch>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <Switch>
+        <PrivateRoute exact path='/admin'>
+          <UserList />
+        </PrivateRoute>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/merci' component={Remerciement} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/formulaire/:id' render={(props) => <Formulaire {...props} byId={true} />} />
+        <Route strict path='/:origine/' component={Formulaire} />
+        <Route component={NotFound} />
+      </Switch>
+    </ChakraProvider>
   )
 }
 

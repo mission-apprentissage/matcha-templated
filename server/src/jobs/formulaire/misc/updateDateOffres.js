f518ad62ea236cdec1e613e0775a72f5a595accf
Kevin Barnoin <k.barnoin@gmail.com>
2021-05-24 15:53:50 +0200

    job: update lbb offre with new schema

---
+++ server/src/jobs/formulaire/misc/updateDateOffres.js
@@ -0,0 +1,29 @@
+const { Formulaire } = require("../../../common/model");
+const { asyncForEach } = require("../../../common/utils/asyncUtils");
+const { paginator } = require("../../../common/utils/paginator");
+const { runScript } = require("../../scriptWrapper");
+
+runScript(async () => {
+  await paginator(Formulaire, {}, { lean: true }, async (formulaire) => {
+    let offres = [];
+
+    await asyncForEach(formulaire.offres, async (offre) => {
+      offres.push({
+        ...offre,
+        date_debut_apprentissage: new Date("2021-09-06"),
+        date_creation: new Date("2021-06-01"),
+        date_expiration: new Date("2021-07-01"),
+        statut: {
+          active: true,
+          filled: false,
+          canceled: false,
+        },
+      });
+    });
+
+    formulaire.origine = "lbb";
+    formulaire.offres = offres;
+
+    await Formulaire.findByIdAndUpdate(formulaire._id, formulaire);
+  });
+});

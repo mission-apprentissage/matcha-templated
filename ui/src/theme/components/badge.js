48650970e4b72e4bb82cb74ec7fe372cbf8b0fca
Kevin Barnoin <k.barnoin@gmail.com>
2021-05-27 21:44:00 +0200

    fix: badge lint

--- ui/src/theme/components/badge.js
+++ ui/src/theme/components/badge.js
@@ -1,58 +1,58 @@
 const commonStatusBadgeStyle = {
-  fontSize: "omega",
-  fontWeight: 400,
+  fontSize: 'omega',
+  fontWeight: 700,
   borderRadius: 20,
-  pl: 1,
-  pr: 3,
+  pl: 4,
+  pr: 4,
   py: 1,
-  textTransform: "none",
-};
+  textTransform: 'none',
+}
 
 const Badge = {
   variants: {
     notRelevant: {
       ...commonStatusBadgeStyle,
-      bg: "#e3e8ea",
-      color: "grey.800",
-      border: "1px solid",
-      borderColor: "#e3e8ea",
+      bg: '#e3e8ea',
+      color: 'grey.800',
+      border: '1px solid',
+      borderColor: '#e3e8ea',
     },
     published: {
       ...commonStatusBadgeStyle,
-      bg: "greenmedium.300",
-      color: "grey.800",
-      border: "1px solid",
-      borderColor: "greenmedium.200",
+      bg: 'greenmedium.300',
+      color: 'grey.800',
+      border: '1px solid',
+      borderColor: 'greenmedium.200',
     },
     notPublished: {
       ...commonStatusBadgeStyle,
-      bg: "grey.300",
-      color: "grey.800",
-      border: "1px solid",
-      borderColor: "grey.300",
+      bg: 'grey.300',
+      color: 'grey.800',
+      border: '1px solid',
+      borderColor: 'grey.300',
     },
     toBePublished: {
       ...commonStatusBadgeStyle,
-      bg: "orangemedium.300",
-      color: "grey.800",
-      border: "1px solid",
-      borderColor: "orangemedium.300",
+      bg: 'orangemedium.300',
+      color: 'grey.800',
+      border: '1px solid',
+      borderColor: 'orangemedium.300',
     },
     pending: {
       ...commonStatusBadgeStyle,
-      bg: "greenmedium.200",
-      color: "#a3b3b7",
-      border: "1px solid",
-      borderColor: "greenmedium.200",
+      bg: 'greenmedium.200',
+      color: '#a3b3b7',
+      border: '1px solid',
+      borderColor: 'greenmedium.200',
     },
     year: {
       ...commonStatusBadgeStyle,
-      bg: "greenmedium.300",
-      color: "grey.800",
-      pl: "15px",
-      pr: "15px",
+      bg: 'greenmedium.300',
+      color: 'grey.800',
+      pl: '15px',
+      pr: '15px',
     },
   },
-};
+}
 
-export { Badge };
+export { Badge }

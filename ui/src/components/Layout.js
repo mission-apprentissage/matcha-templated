955c190292949819237402a88dc177335bf55ce0
Kevin Barnoin <k.barnoin@gmail.com>
2021-05-30 23:23:19 +0200

    fix: layout footer

--- ui/src/components/Layout.js
+++ ui/src/components/Layout.js
@@ -1,21 +1,20 @@
-import { Box, Container, Divider } from '@chakra-ui/react'
+import { Box, Container, Divider, Spacer, Flex } from '@chakra-ui/react'
 import { Footer, Mission } from '../pages/HomePage'
 import Navbar from './Navbar'
 
-export default ({ children, background, footer = false }) => {
+export default ({ children, background }) => {
   return (
     <Container maxW='full' p='0'>
-      <Navbar />
-      <Box bg={background ?? 'lightGrey'} h='100vh'>
-        {children}
-      </Box>
-      {footer && (
+      <Flex direction='column'>
+        <Navbar />
+        <Box bg={background ?? 'lightGrey'}>{children}</Box>
+        <Spacer />
         <Box>
           <Mission />
           <Divider />
           <Footer />
         </Box>
-      )}
+      </Flex>
     </Container>
   )
 }

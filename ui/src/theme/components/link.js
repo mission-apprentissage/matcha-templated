1db2add3c8f576a54bafcca758d1343074bb60ae
Kevin Barnoin <k.barnoin@gmail.com>
2021-07-13 18:39:09 +0200

    feat: link variant

--- ui/src/theme/components/link.js
+++ ui/src/theme/components/link.js
@@ -2,6 +2,11 @@ const Link = {
   baseStyle: {
     _focus: { boxShadow: '0 0 0 3px #000091', outlineColor: 'bluefrance.500' },
   },
+  variants: {
+    unstyled: {
+      _focus: { boxShadow: 'none', outlineColor: 'none' },
+    },
+  },
 }
 
 export { Link }

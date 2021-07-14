feb6a5364116d04a87a560ee700acda3d04af87c
Kevin Barnoin <k.barnoin@gmail.com>
2021-07-14 23:13:13 +0200

    feat: ui users mgmt (wip)

--- ui/src/api.js
+++ ui/src/api.js
@@ -10,17 +10,27 @@ const errorHandler = (error) => {
   }
 }
 
+/**
+ * Formulaire API
+ */
 export const getFormulaire = (formId) => API.get(`/formulaire/${formId}`).catch(errorHandler)
-
 export const postFormulaire = (form) => API.post(`/formulaire`, form).catch(errorHandler)
-
 export const putFormulaire = (formId, form) => API.put(`/formulaire/${formId}`, form).catch(errorHandler)
 
+/**
+ * Offre API
+ */
 export const postOffre = (formId, offre) => API.post(`/formulaire/${formId}/offre`, offre).catch(errorHandler)
-
 export const putOffre = (offreId, offre) => API.put(`/formulaire/offre/${offreId}`, offre).catch(errorHandler)
 
-export const getWithQS = (payload) =>
-  API.get('/formulaire', { params: { query: JSON.stringify(payload.query), ...payload } })
+/**
+ * User API
+ */
+export const getUsers = async () => await API.get('/user').catch(errorHandler)
+export const createUser = async (user) => await API.post('/user', user).catch(errorHandler)
+export const updateUser = async (userId, user) => await API.put(`user/${userId}`, user).catch(errorHandler)
+export const deleteUser = async (userId) => await API.delete(`/user/${userId}`).catch(errorHandler)
 
 export const getSiretInfo = async (siret) => await API.get(`/entreprise/${siret}`).catch(errorHandler)
+export const getWithQS = (payload) =>
+  API.get('/formulaire', { params: { query: JSON.stringify(payload.query), ...payload } })
--- ui/src/pages/Admin/Users.js
+++ ui/src/pages/Admin/Users.js
@@ -1,12 +1,92 @@
 import { Link } from 'react-router-dom'
-import { Box, Container, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
+import {
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Container,
+  Box,
+  Center,
+  Icon,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Spacer,
+  Flex,
+  Text,
+  Badge,
+  useEditableControls,
+  Editable,
+  EditablePreview,
+  EditableInput,
+  ButtonGroup,
+  IconButton,
+  useToast,
+} from '@chakra-ui/react'
+import moment from 'moment'
+import { AiOutlineEdit, AiOutlineRight, AiOutlineCheck, AiOutlineClose } from 'react-icons/ai'
 
 import { Layout } from '../../components'
 import { ArrowDropRightLine } from '../../theme/components/icons/'
+import { useEffect, useState } from 'react'
+import { getUsers } from '../../api'
+
+const MyTable = ({ users }) => {
+  if (users.length < 0) return <div>Chargement en cours</div>
+
+  const Date = (date) => moment(date).format('DD/MM/YYYY')
+
+  return (
+    <Box py='4'>
+      <Table size='sm' bg='white'>
+        <Thead borderBottom='2px solid grey'>
+          <Th>Admin</Th>
+          <Th py={6} paddingLeft='30px'>
+            Username
+          </Th>
+          <Th>Email</Th>
+          <Th>Organisation</Th>
+          <Th>Scope des offres visibles (Origine)</Th>
+          <Th>Edition</Th>
+        </Thead>
+        <Tbody>
+          {users?.map((item, index) => {
+            return (
+              <Tr key={index}>
+                <Td>{item.isAdmin ? 'Oui' : 'Non'}</Td>
+                <Td py={4} paddingLeft='30px'>
+                  {item.username}
+                </Td>
+                <Td>{item.email}</Td>
+                <Td>{item.organization}</Td>
+                <Td>{item.scope}</Td>
+                <Td>
+                  <Center>
+                    <Link to={`/user/${item.id_form}`} target='_blank'>
+                      <Icon color='bluefrance.500' w={5} h={5} as={AiOutlineEdit} />
+                    </Link>
+                  </Center>
+                </Td>
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+    </Box>
+  )
+}
 
 export default () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getUsers().then((users) => setUsers(users.data))
+  })
+
   return (
-    <Layout>
+    <Layout background='beige'>
       <Container maxW='container.xl' py={4}>
         <Box pt={3}>
           <Breadcrumb separator={<ArrowDropRightLine color='grey.600' />} textStyle='xs'>
@@ -26,6 +106,8 @@ export default () => {
         <Box textStyle='h3' fontSize={['sm', '3xl']} fontWeight='700' color='grey.800' py={3}>
           Gestion des utilisateurs
         </Box>
+
+        <MyTable users={users} />
       </Container>
     </Layout>
   )
